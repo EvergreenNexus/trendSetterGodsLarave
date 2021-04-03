6dd977f3cade99556445bb688579d0b0e80991f6
ChaTbiM <chatbimdev@gmail.com>
2021-04-03 19:41:47 +0200

    initial commit with header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,110 @@
+<header>
+    <div class="modal" tabindex="1" id="myModal" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Welcome To TrendSetterGods !</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body text-center">
+                    <h4>We provide free delivery for all local customers from Everett to Centralia</h4>
+                    <h3 class="text-primary ">Call 253-592-0505</h3>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $('#myModal').modal('show');
+
+    </script>
+    <!--  -->
+    <div class="carousel fade-carousel slide carousel-fade" data-interval="7000" data-ride="carousel" id="bs-carousel">
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="carousel-item slides item active">
+                <div class="slide-1"></div>
+
+                <div class="hero">
+
+                    <header>
+                        <img id="logo" src="{{asset('images/TSG_fulllogo_noborder.png')}}">
+                    </header>
+
+
+                    <div>
+                        <a class="btn" href="/" id="nav">Home</a>
+                        <a class="btn" href="#apparel" id="nav">Apparel</a>
+                        <a class="btn" href="#mens" id="nav">Men's</a>
+                        <a class="btn" href="#womens" id="nav">Women's</a>
+                        <a class="btn" href="#youth" id="nav">Youth</a>
+                        <a class="btn" href="#used" id="nav">Used</a>
+
+                        <a href="https://www.facebook.com/trendsetter.jesus" target="_blank"> <img id="social"
+                                src="{{asset('images/facebooksml.png')}}"></a>
+                        <a href="https://www.instagram.com/trendsettergods2020/" target="_blank"><img id="social"
+                                src="{{asset('images/instagramsml.png')}}"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item slides item">
+                <div class="slide-2"></div>
+
+                <div class="hero">
+
+
+                    <header><img id="logo" src="{{asset('images/TSG_fulllogo_noborder.png')}}"></header>
+                    <div>
+                        <a class="btn" href="/" id="nav">Home</a>
+                        <a class="btn" href="#apparel" id="nav">Apparel</a>
+                        <a class="btn" href="#mens" id="nav">Men's</a>
+                        <a class="btn" href="#womens" id="nav">Women's</a>
+                        <a class="btn" href="#youth" id="nav">Youth</a>
+                        <a class="btn" href="#used" id="nav">Used</a>
+
+                        <a href="https://www.facebook.com/trendsetter.jesus" target="_blank"> <img id="social"
+                                src="{{asset('images/facebooksml.png')}}"></a>
+                        <a href="https://www.instagram.com/trendsettergods2020/" target="_blank"><img id="social"
+                                src="{{asset('images/instagramsml.png')}}"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item slides item">
+                <div class="slide-3"></div>
+
+                <div class="hero">
+
+
+                    <header><img id="logo" src="{{asset('images/TSG_fulllogo_noborder.png')}}"></header>
+                    <div> <a class="btn" href="/" id="nav">Home</a>
+                        <a class="btn" href="#apparel" id="nav">Apparel</a>
+                        <a class="btn" href="#mens" id="nav">Men's</a>
+                        <a class="btn" href="#womens" id="nav">Women's</a>
+                        <a class="btn" href="#youth" id="nav">Youth</a>
+                        <a class="btn" href="#used" id="nav">Used</a>
+
+                        <a href="https://www.facebook.com/trendsetter.jesus" target="_blank"> <img id="social"
+                                src="{{asset('images/facebooksml.png')}}"></a>
+                        <a href="https://www.instagram.com/trendsettergods2020/" target="_blank"><img id="social"
+                                src="{{asset('images/instagramsml.png')}}"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="top"></div>
+
+
+    {{-- <audio style="margin: 0 auto; display: block;" id="bgmusic" src="{{asset('music/Mike-Deez-Got-Swag.mp3')}}" controls=""
+        autoplay="" loop="loop">
+    </audio> --}}
+</header>
