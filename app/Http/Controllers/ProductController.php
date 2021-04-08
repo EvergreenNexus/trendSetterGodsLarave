01e052fdfff90264edb0bcc5f9fa25f63cbf4ee6
ChaTbiM <chatbimdev@gmail.com>
2021-04-09 00:22:34 +0200

    finished dynamic variation product adding

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,10 +17,10 @@ class ProductController extends Controller
         }
 
         $men_products = Product::where('category', 'men')->get()->load('variations')->toArray();
-        $women_products = Product::where('category','women')->get()->load('variations')->toArray();
-        $youth_products = Product::where('category','youth')->get()->load('variations')->toArray();
-        $apparel_products = Product::where('category','apparel')->get()->load('variations')->toArray();
-        $used_products = Product::where('category','used')->get()->load('variations')->toArray();
+        $women_products = Product::where('category', 'women')->get()->load('variations')->toArray();
+        $youth_products = Product::where('category', 'youth')->get()->load('variations')->toArray();
+        $apparel_products = Product::where('category', 'apparel')->get()->load('variations')->toArray();
+        $used_products = Product::where('category', 'used')->get()->load('variations')->toArray();
 
         $cachedPage = (string) view('index', [
             'men_products' => $men_products,
@@ -28,20 +28,24 @@ class ProductController extends Controller
             'youth_products' => $youth_products,
             'used_products' => $used_products,
             'apparel_products' => $apparel_products,
-            ])->render();
+        ])->render();
         File::put(storage_path('/app/public/products.html'), $cachedPage);
 
 
         // Storage::url('images/air-jordan-3-retro-se-fire-red.jpg');
 
 
-        return view('index' , ['men_products'=> $men_products]);
+        return view('index', ['men_products' => $men_products]);
     }
 
     public function store(Request $request)
     {
+
         $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'name' => 'required',
+            'price' => 'required',
+            'category' => 'required'
         ]);
 
 
@@ -53,20 +57,23 @@ class ProductController extends Controller
         $product->quantity = $request->quantity;
         $product->category = $request->category;
 
-        
+
+        // Image Storing
         $imageName = $request->image->getClientOriginalName();
-        $path = $request->image->storeAs('/images', $imageName , 'public');
-        $product->image = 'storage/'.$path;
+        $path = $request->image->storeAs('/images', $imageName, 'public');
+        $product->image = 'storage/' . $path;
         // $product = Storage::url('public/images/.air-jordan-3-retro-se-fire-red.jpg');
         // $product = Storage::url('public')->get('images/air-jordan-3-retro-se-fire-red.jpg');
 
         // $product = Storage::disk('public')->get('images/air-jordan-3-retro-se-fire-red.jpg');
-        // dd($path);
         try {
             $product->save();
-            return back()->with('success', 'product was created successfully');
         } catch (\Throwable $th) {
             return back()->with('failure', 'error while creating a product');
         }
+
+        // Trigger Page Home page Caching and redirect . 
+        return back()->with('success', 'product was created successfully');
+
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,50 +25,130 @@
                         <option>used</option>
                     </select>
                 </div>
+                <div class="container-fluid pl-0" id="variationsContainer">
+                    <span class="add_variation row">
+                        <div class="form-group col">
+                            <label for="size">Size</label>
+                            <select class="form-control" name="size[]" id="size">
+                                <option>8</option>
+                                <option>8.5</option>
+                                <option>9</option>
+                                <option>9.5</option>
+                                <option>10</option>
+                                <option>10.5</option>
+                                <option>11</option>
+                            </select>
+                        </div>
+                        <div class="form-group col">
+                            <label for="quantity">quantity</label>
+                            <input name="quantity[]" placeholder="enter product quentity" type="text" class="form-control"
+                                id="quantity">
+                        </div>
+                        <div class="form-group col d-flex align-items-end" id="button__container">
+                            <svg data-toggle="tooltip" data-placement="top" title="Add Product Variations"
+                                xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="green"
+                                class="bi bi-plus-square add__variation" viewBox="0 0 16 16">
+                                <path
+                                    d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                                <path
+                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                            </svg>
+                        </div>
+                    </span>
+                </div>
                 <div class="form-group">
                     <label for="image">Product Picture</label>
                     <input type="file" name="image" class="form-control-file" id="image">
                 </div>
-                <div class="form-group w-25">
-                    <label for="size">Size</label>
-                    <select class="form-control" name="size" id="size">
-                        <option>8</option>
-                        <option>8.5</option>
-                        <option>9</option>
-                        <option>9.5</option>
-                        <option>10</option>
-                        <option>10.5</option>
-                        <option>11</option>
-                    </select>
-                </div>
-                <div class="form-group w-25">
-                    <label for="quantity">quantity</label>
-                    <input name="quantity" placeholder="enter product quentity" type="text" class="form-control"
-                        id="quantity">
-                </div>
+                <button type="submit" class="btn btn-primary ">Create Product</button>
+                <button type="submit" id="clear" class="btn btn-danger ">Clear</button>
 
-                <button type="submit" class="btn btn-primary">Create Product</button>
             </form>
         </div>
+
     </div>
-    
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-        <div class="toast-header">
-            {{-- <img src="..." class="rounded mr-2" alt="..."> --}}
-            <strong class="mr-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+
+    {{-- Success Toast --}}
+    @isset($success)
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+            <div class="toast-header">
+                <strong class="mr-auto">Product Created</strong>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <small>1 sec ago</small>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="toast-body">
+                Please update website ... !
+            </div>
         </div>
-        <div class="toast-body">
-            Hello, world! This is a toast message.
-        </div>
-    </div>
+    @endisset
+
     <script>
         $(document).ready(function() {
             $('.toast').toast('show');
+
+            $('.toast').on('shown.bs.toast', function() {
+                setTimeout(() => {
+                    $('.toast').toast('hide');
+                }, 3000);
+            })
+
+            $('.clear').click(function(e) {
+                $('.form').trigger('reset');
+            })
+
+
+            let addVariationButton = $('.add__variation');
+            let removeVariationButton = $('.remove__variation');
+
+            addVariationButton.tooltip();
+            removeVariationButton.tooltip();
+
+            // Add More variations ( size and quantity inputs)
+            addVariationButton.click(() => {
+                let removeVariationButton = `
+                                            <span  class="remove__variation row">
+                                                <div class="form-group col">
+                                                    <label for="size">Size</label>
+                                                    <select class="form-control" name="size[]" id="size">
+                                                        <option>8</option>
+                                                        <option>8.5</option>
+                                                        <option>9</option>
+                                                        <option>9.5</option>
+                                                        <option>10</option>
+                                                        <option>10.5</option>
+                                                        <option>11</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group col">
+                                                    <label for="quantity">quantity</label>
+                                                    <input name="quantity[]" placeholder="enter product quentity" type="text" class="form-control"
+                                                        id="quantity">
+                                                </div>
+                                                <div class="form-group col d-flex align-items-end remove__variation__button" >
+                                                    <svg data-toggle="tooltip" data-placement="top" title="Remove Product Variations"
+                                                        xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="red"
+                                                        class=" bi bi-dash-square remove__variation" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                                                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                                                    </svg>
+                                                </div>
+                                            </span>
+                                            `
+                $('#variationsContainer').append(removeVariationButton);
+            })
+
+            $('#variationsContainer').on('click', '.remove__variation__button', function() {
+                this.closest('.remove__variation').remove();
+            })
+
         });
+
     </script>
 
 @endsection
