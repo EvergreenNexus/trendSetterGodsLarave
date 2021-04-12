7ef43ddbc404902069b1c48668fab1af6474a0bc
ChaTbiM <chatbimdev@gmail.com>
2021-04-12 16:02:45 +0200

    updated product creation error/success  messages

--- resources/views/dashboard/create-product-errors.blade.php
+++ resources/views/dashboard/create-product-errors.blade.php
@@ -1,3 +1,72 @@
-@error('name')
-    <div class="alert alert-danger">{{ $message }}</div>
-@enderror
+{{-- Success Toast --}}
+@if (session('success'))
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+        <div class="toast-header text-white bg-success">
+            <strong class="mr-auto">Product Created</strong>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <small>1 sec ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            <p>{{ session('success') }}</p>
+            <p>
+                Please update website ... !
+            </p>
+        </div>
+    </div>
+@endif
+
+{{-- error while creating a product --}}
+@if (session('failure'))
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+        <div class="toast-header text-white bg-danger">
+            <strong class="mr-auto">Product Not Created !</strong>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <small>1 sec ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            <p>{{ session('failure') }}</p>
+            <p>Please try again ..</p>
+        </div>
+    </div>
+@endif
+
+{{-- input errors --}}
+@if ($errors->any())
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+        <div class="toast-header text-white bg-danger">
+            <strong class="mr-auto">Failed</strong>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <small>1 sec ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            <ul class="  pl-0 text-center">
+                @foreach ($errors->all() as $error)
+                    @if (preg_match('/(quantity)/i', $error))
+                        <li class=" alert alert-danger">The quantity field is required</li>
+                    @elseif(preg_match('/(size)/i', $error))
+                        <li class=" alert alert-danger">The size field is required</li>
+                    @else
+                        <li class=" alert alert-danger">{{ $error }}</li>
+                    @endif
+                @endforeach
+            </ul>
+        </div>
+    </div>
+
+
+@endif
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,4 @@
 @extends('dashboard.layout')
-
 @section('content')
     <div class="card">
         <div class="card-body ">
@@ -77,55 +76,7 @@
 
     </div>
 
-    {{-- Success Toast --}}
-    @isset($success)
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-            <div class="toast-header">
-                <strong class="mr-auto">Product Created</strong>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <small>1 sec ago</small>
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="toast-body">
-                Please update website ... !
-            </div>
-        </div>
-    @endisset
-    {{-- errors --}}
-    @if ($errors->any())
-
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-            <div class="toast-header">
-                <strong class="mr-auto">Failed</strong>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <small>1 sec ago</small>
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="toast-body">
-                <ul class="  pl-0 text-center">
-                    @foreach ($errors->all() as $error)
-                        @if (preg_match('/(quantity)/i', $error))
-                            <li class=" alert alert-danger">The quantity field is required</li>
-                        @elseif(preg_match('/(size)/i', $error))
-                            <li class=" alert alert-danger">The size field is required</li>
-                        @else
-                            <li class=" alert alert-danger">{{ $error }}</li>
-                        @endif
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-
-
-    @endif
+    @include('dashboard.create-product-errors')
 
     <script>
         $(document).ready(function() {
@@ -134,7 +85,7 @@
             $('.toast').on('shown.bs.toast', function() {
                 setTimeout(() => {
                     $('.toast').toast('hide');
-                }, 3000);
+                }, 4000);
             })
 
             $('.clear').click(function(e) {
@@ -151,36 +102,36 @@
             // Add More variations ( size and quantity inputs)
             addVariationButton.click(() => {
                 let removeVariationButton = `
-                                                                                <span  class="remove__variation row">
-                                                                                    <div class="form-group col-2">
-                                                                                        <label for="size">Size</label>
-                                                                                        <select required class="form-control" name="size[]" id="size">
-                                                                                            <option disabled selected value> -- size -- </option>
-                                                                                            <option>8</option>
-                                                                                            <option>8.5</option>
-                                                                                            <option>9</option>
-                                                                                            <option>9.5</option>
-                                                                                            <option>10</option>
-                                                                                            <option>10.5</option>
-                                                                                            <option>11</option>
-                                                                                        </select>
-                                                                                    </div>
-                                                                                    <div class="form-group col-3">
-                                                                                        <label for="quantity">quantity</label>
-                                                                                        <input required name="quantity[]" placeholder="product quantity..." type="text" class="form-control"
-                                                                                            id="quantity">
-                                                                                    </div>
-                                                                                    <div class="form-group col-1 d-flex align-items-end" >
-                                                                                        <svg data-toggle="tooltip" data-placement="top" title="Remove Product Variations"
-                                                                                            xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="red"
-                                                                                            class=" bi bi-dash-square remove__variation__button" viewBox="0 0 16 16">
-                                                                                            <path
-                                                                                                d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                                                                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
-                                                                                        </svg>
-                                                                                    </div>
-                                                                                </span>
-                                                                                `
+                                                                                                <span  class="remove__variation row">
+                                                                                                    <div class="form-group col-2">
+                                                                                                        <label for="size">Size</label>
+                                                                                                        <select required class="form-control" name="size[]" id="size">
+                                                                                                            <option disabled selected value> -- size -- </option>
+                                                                                                            <option>8</option>
+                                                                                                            <option>8.5</option>
+                                                                                                            <option>9</option>
+                                                                                                            <option>9.5</option>
+                                                                                                            <option>10</option>
+                                                                                                            <option>10.5</option>
+                                                                                                            <option>11</option>
+                                                                                                        </select>
+                                                                                                    </div>
+                                                                                                    <div class="form-group col-3">
+                                                                                                        <label for="quantity">quantity</label>
+                                                                                                        <input required name="quantity[]" placeholder="product quantity..." type="number" class="form-control"
+                                                                                                            id="quantity">
+                                                                                                    </div>
+                                                                                                    <div class="form-group col-1 d-flex align-items-end" >
+                                                                                                        <svg data-toggle="tooltip" data-placement="top" title="Remove Product Variations"
+                                                                                                            xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="red"
+                                                                                                            class=" bi bi-dash-square remove__variation__button" viewBox="0 0 16 16">
+                                                                                                            <path
+                                                                                                                d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                                                                                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                                                                                                        </svg>
+                                                                                                    </div>
+                                                                                                </span>
+                                                                                                `
                 $('#variationsContainer').append(removeVariationButton);
             })
 
